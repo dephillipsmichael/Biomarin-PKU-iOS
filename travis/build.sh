fda51c798c542db4749364dd9094e9cf6a79ab79
syoung-smallwisdom <shannon.young@sagebase.org>
2019-05-02 14:06:04 -0700

    Stub out adding travis

---
+++ travis/build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -ex
+if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then     # on pull requests
+    echo "Build on PR"
+    FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane test scheme:"BiomarinPKUTestApp"
+elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" == "master" ]]; then  # non-tag commits to master branch
+    echo "Build on merge to master"
+    git clone https://github.com/Sage-Bionetworks/iOSPrivateProjectInfo.git ../iOSPrivateProjectInfo
+    FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane test scheme:"BiomarinPKU"
+    bundle exec fastlane keychains
+    bundle exec fastlane certificates
+    bundle exec fastlane ci_archive scheme:"BiomarinPKU" export_method:"app-store" project:"BiomarinPKU/BiomarinPKU.xcodeproj"
+elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" =~ ^stable-.* ]]; then # non-tag commits to stable branches
+    echo "Build on stable branch"
+    git clone https://github.com/Sage-Bionetworks/iOSPrivateProjectInfo.git ../iOSPrivateProjectInfo
+    FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane test scheme:"BiomarinPKU"
+    bundle exec fastlane keychains
+    bundle exec fastlane certificates
+    bundle exec fastlane beta scheme:"BiomarinPKU" export_method:"app-store" project:"BiomarinPKU/BiomarinPKU.xcodeproj"
+fi
+exit $?
