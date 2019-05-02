16b6306998e311876fb708dfb5488332b0bbd9d0
syoung-smallwisdom <shannon.young@sagebase.org>
2019-05-02 15:10:19 -0700

    Add stubbed out app to workspace

--- travis/build.sh
+++ travis/build.sh
@@ -2,7 +2,8 @@
 set -ex
 if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then     # on pull requests
     echo "Build on PR"
-    FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane test scheme:"BiomarinPKUTestApp"
+    # syoung 05/02/2019 If you wish to run tests on pull requests, you will need to set up a test app that does not hook to bridge.
+    # FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane test scheme:"BiomarinPKUTestApp"
 elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" == "master" ]]; then  # non-tag commits to master branch
     echo "Build on merge to master"
     git clone https://github.com/Sage-Bionetworks/iOSPrivateProjectInfo.git ../iOSPrivateProjectInfo
