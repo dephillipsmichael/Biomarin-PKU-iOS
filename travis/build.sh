48f90776824b7b5fdcac94956da274c8cac37e08
syoung-smallwisdom <shannon.young@sagebase.org>
2019-05-03 12:10:17 -0700

    comment out build and archive of the project

--- travis/build.sh
+++ travis/build.sh
@@ -6,11 +6,11 @@ if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then     # on pull requests
     # FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane test scheme:"BiomarinPKUTestApp"
 elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" == "master" ]]; then  # non-tag commits to master branch
     echo "Build on merge to master"
-    git clone https://github.com/Sage-Bionetworks/iOSPrivateProjectInfo.git ../iOSPrivateProjectInfo
-    FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane test scheme:"BiomarinPKU"
-    bundle exec fastlane keychains
-    bundle exec fastlane certificates
-    bundle exec fastlane ci_archive scheme:"BiomarinPKU" export_method:"app-store" project:"BiomarinPKU/BiomarinPKU.xcodeproj"
+    # git clone https://github.com/Sage-Bionetworks/iOSPrivateProjectInfo.git ../iOSPrivateProjectInfo
+    # FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane test scheme:"BiomarinPKU"
+    # bundle exec fastlane keychains
+    # bundle exec fastlane certificates
+    # bundle exec fastlane ci_archive scheme:"BiomarinPKU" export_method:"app-store" project:"BiomarinPKU/BiomarinPKU.xcodeproj"
 elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" =~ ^stable-.* ]]; then # non-tag commits to stable branches
     echo "Build on stable branch"
     git clone https://github.com/Sage-Bionetworks/iOSPrivateProjectInfo.git ../iOSPrivateProjectInfo
