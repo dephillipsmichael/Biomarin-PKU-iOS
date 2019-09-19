34eed316e56b3469113185e15db812b682227185
MDP <mike@sdpdigital.com>
2019-09-19 09:11:53 -0400

    Edge case permission fix

--- BiomarinPKU/BiomarinPKU-Study/ActivityViewController.swift
+++ BiomarinPKU/BiomarinPKU-Study/ActivityViewController.swift
@@ -261,6 +261,11 @@ class ActivityViewController: UIViewController, RSDTaskViewControllerDelegate {
     }
     
     @IBAction func physicalTapped() {
+        // Work-around fix for permission bug
+        // This will force the overview screen to check permission state every time
+        // Usually research framework caches it and the state becomes invalid
+        UserDefaults.standard.removeObject(forKey: "rsd_MotionAuthorizationStatus")
+        
         self.presentTaskViewController(for: .physical)
     }
     
